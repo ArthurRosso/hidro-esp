1a78a103e2ace6fe61562209ec1137b7be73fd14
ArthurRosso <arthurderosso@gmail.com>
2021-06-16 20:49:36 -0300

    add hygrometer

--- main/main.c
+++ main/main.c
@@ -24,21 +24,143 @@
 
 #include "esp_http_client.h"
 
+#include "freertos/portmacro.h"
+#include "freertos/queue.h"
+#include "driver/periph_ctrl.h"
+#include "driver/ledc.h"
+#include "driver/gpio.h"
+#include "driver/pcnt.h"
+#include "esp_attr.h"
+
 #include "station.h"
 
+#define PCNT_TEST_UNIT      PCNT_UNIT_0
+#define PCNT_H_LIM_VAL      20
+#define PCNT_THRESH1_VAL    5
+#define PCNT_INPUT_SIG_IO   4  // Pulse Input GPIO
+#define PCNT_INPUT_CTRL_IO  5  // Control GPIO HIGH=count up, LOW=count down
+#define LEDC_OUTPUT_IO      18 // Output GPIO of a sample 1 Hz pulse generator
+
+xQueueHandle pcnt_evt_queue;   // A queue to handle pulse counter events
+pcnt_isr_handle_t user_isr_handle = NULL; //user's ISR service handle
 static const char *TAG = "HTTP_CLIENT";
+
+/* A sample structure to pass events from the PCNT
+ * interrupt handler to the main program.
+ */
+typedef struct {
+    int unit;  // the PCNT unit that originated an interrupt
+    uint32_t status; // information on the event type that caused the interrupt
+} pcnt_evt_t;
+
+/* Decode what PCNT's unit originated an interrupt
+ * and pass this information together with the event type
+ * the main program using a queue.
+ */
+static void IRAM_ATTR pcnt_example_intr_handler(void *arg)
+{
+    uint32_t intr_status = PCNT.int_st.val;
+    int i;
+    pcnt_evt_t evt;
+    portBASE_TYPE HPTaskAwoken = pdFALSE;
+
+    for (i = 0; i < PCNT_UNIT_MAX; i++) {
+        if (intr_status & (BIT(i))) {
+            evt.unit = i;
+            /* Save the PCNT event type that caused an interrupt
+               to pass it to the main program */
+            evt.status = PCNT.status_unit[i].val;
+            PCNT.int_clr.val = BIT(i);
+            xQueueSendFromISR(pcnt_evt_queue, &evt, &HPTaskAwoken);
+            if (HPTaskAwoken == pdTRUE) {
+                portYIELD_FROM_ISR();
+            }
+        }
+    }
+}
+
+/* Configure LED PWM Controller
+ * to output sample pulses at 1 Hz with duty of about 10%
+ */
+static void ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer;
+    ledc_timer.speed_mode       = LEDC_LOW_SPEED_MODE;
+    ledc_timer.timer_num        = LEDC_TIMER_1;
+    ledc_timer.duty_resolution  = LEDC_TIMER_10_BIT;
+    ledc_timer.freq_hz          = 1;  // set output frequency at 1 Hz
+    ledc_timer.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&ledc_timer);
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel;
+    ledc_channel.speed_mode = LEDC_LOW_SPEED_MODE;
+    ledc_channel.channel    = LEDC_CHANNEL_1;
+    ledc_channel.timer_sel  = LEDC_TIMER_1;
+    ledc_channel.intr_type  = LEDC_INTR_DISABLE;
+    ledc_channel.gpio_num   = LEDC_OUTPUT_IO;
+    ledc_channel.duty       = 100; // set duty at about 10%
+    ledc_channel.hpoint     = 0;
+    ledc_channel_config(&ledc_channel);
+}
+
+/* Initialize PCNT functions:
+ *  - configure and initialize PCNT
+ *  - set up the input filter
+ *  - set up the counter events to watch
+ */
+static void pcnt_example_init(void)
+{
+    /* Prepare configuration for the PCNT unit */
+    pcnt_config_t pcnt_config = {
+        // Set PCNT input signal and control GPIOs
+        .pulse_gpio_num = PCNT_INPUT_SIG_IO,
+        .ctrl_gpio_num = PCNT_INPUT_CTRL_IO,
+        .channel = PCNT_CHANNEL_0,
+        .unit = PCNT_TEST_UNIT,
+        // What to do on the positive / negative edge of pulse input?
+        .pos_mode = PCNT_COUNT_INC,   // Count up on the positive edge
+        .neg_mode = PCNT_COUNT_DIS,   // Keep the counter value on the negative edge
+        // What to do when control input is low or high?
+        .lctrl_mode = PCNT_MODE_REVERSE, // Reverse counting direction if low
+        .hctrl_mode = PCNT_MODE_KEEP,    // Keep the primary counter mode if high
+        // Set the maximum and minimum limit values to watch
+        .counter_h_lim = PCNT_H_LIM_VAL,
+    };
+    /* Initialize PCNT unit */
+    pcnt_unit_config(&pcnt_config);
+
+    /* Configure and enable the input filter */
+    pcnt_set_filter_value(PCNT_TEST_UNIT, 1023); // maior número possível
+    pcnt_filter_enable(PCNT_TEST_UNIT);
+
+    /* Set threshold 0 and 1 values and enable events to watch */
+    pcnt_set_event_value(PCNT_TEST_UNIT, PCNT_EVT_THRES_1, PCNT_THRESH1_VAL);
+    pcnt_event_enable(PCNT_TEST_UNIT, PCNT_EVT_THRES_1);
+    /* Enable events on zero, maximum and minimum limit values */
+    pcnt_event_enable(PCNT_TEST_UNIT, PCNT_EVT_ZERO);
+    pcnt_event_enable(PCNT_TEST_UNIT, PCNT_EVT_H_LIM);
+    pcnt_event_enable(PCNT_TEST_UNIT, PCNT_EVT_L_LIM);
+
+    /* Initialize PCNT's counter */
+    pcnt_counter_pause(PCNT_TEST_UNIT);
+    pcnt_counter_clear(PCNT_TEST_UNIT);
+
+    /* Register ISR handler and enable interrupts for PCNT unit */
+    pcnt_isr_register(pcnt_example_intr_handler, NULL, 0, &user_isr_handle);
+    pcnt_intr_enable(PCNT_TEST_UNIT);
+
+    /* Everything is set up, now go to counting */
+    pcnt_counter_resume(PCNT_TEST_UNIT);
+}
 
 void custom_sleep(int ms)
 {
     vTaskDelay(ms / portTICK_PERIOD_MS);
 }
 
-float get_medicao()
-{
-    return esp_timer_get_time()/3.14;
-}
-
-esp_err_t post_data (){
+esp_err_t post_data (int16_t *count){
 
     esp_err_t err;
     char response[200];
@@ -61,7 +183,12 @@ esp_err_t post_data (){
       }
     }
 
-    snprintf(response, sizeof(response), "{\"Mac\": \"%s\", \"Litros\": %f}", strmac, get_medicao());
+    pcnt_get_counter_value(PCNT_TEST_UNIT, count);
+    printf("Current counter value :%d\n", *count);
+
+    snprintf(response, sizeof(response), "{\"Mac\": \"%s\", \"Litros\": %d}", strmac, *count);
+
+    pcnt_counter_clear(PCNT_TEST_UNIT);
 
     //const char *post_data = "{\"field1\":\"value1\"}";
     
@@ -84,11 +211,20 @@ esp_err_t post_data (){
 
 void app_main(void)
 {
+    /* Initialize LEDC to generate sample pulse signal */
+    ledc_init();
+
+    /* Initialize PCNT event queue and PCNT functions */
+    pcnt_evt_queue = xQueueCreate(10, sizeof(pcnt_evt_t));
+    pcnt_example_init();
+
+    int16_t count = 0;
+
     // connect to internet
     ESP_ERROR_CHECK(app_station());
 
     while (1){
-       ESP_ERROR_CHECK(post_data());
+       ESP_ERROR_CHECK(post_data(&count));
        custom_sleep(2000); // 2 segundos
     }
 }
